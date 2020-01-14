cb639defce12ff99b4488501b89afe365b91f84f
Mucyo Fred <mucyomiller@gmail.com>
2020-01-14 18:13:22 +0200

    further explanation

--- 13-scoped-pointer/main.cc
+++ 13-scoped-pointer/main.cc
@@ -27,7 +27,8 @@ int main(int argc, char const *argv[])
   // here c++ do implicity type conversion from Person to ScopedPtr 
   // as long as we goes out of scope our new Person() we'll be deleted out of heap automatically
   // ScopedPtr is handling that for us we don't need to do it again
-  ScopedPtr e = new Person();
+  ScopedPtr e = new Person(); 
+  // usually the above code equal to this  ScopedPtr e(new Person()); or ScopedPtr e = ScopedPtr(new Person());
   // no need for this here -> delete e;
   return 0;
 }
