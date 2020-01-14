4f3c16501984af6fe0d2d465cc30b7b605a64771
Mucyo Fred <mucyomiller@gmail.com>
2020-01-14 18:06:05 +0200

    automatically destruction of heap allocated object :zap:

---
+++ 13-scoped-pointer/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using String = std::string;
+
+class Person
+{
+private:
+  String names;
+
+public:
+  Person() : names("") {}
+  const String &getNames() const { return names; }
+};
+
+class ScopedPtr
+{
+private:
+  Person *p;
+
+public:
+  ScopedPtr(Person *ptr) : p(ptr) {}
+  ~ScopedPtr() { delete p; }
+};
+
+int main(int argc, char const *argv[])
+{
+  // here c++ do implicity type conversion from Person to ScopedPtr 
+  // as long as we goes out of scope our new Person() we'll be deleted out of heap automatically
+  // ScopedPtr is handling that for us we don't need to do it again
+  ScopedPtr e = new Person();
+  // no need for this here -> delete e;
+  return 0;
+}
