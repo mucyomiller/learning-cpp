eaa63dff7868eb738a32f78357a94db97cee8178
Mucyo Fred <mucyomiller@gmail.com>
2020-01-17 14:31:17 +0200

    finish smart pointers

--- 14-smart-pointers/main.cc
+++ 14-smart-pointers/main.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <memory>
-#include<algorithm>
+
 
 using String = std::string;
 
@@ -19,16 +19,16 @@ public:
 };
 
 int main(int argc, const char *argv[])
-{ 
-  std::unique_ptr<Person> person = std::make_unique<Person>(); // expection safety 
+{
+  std::unique_ptr<Person> person = std::make_unique<Person>(); // expection safety
   person->Print();
   // if you don't mind about expections the u can do = new Person(); but you should :) care!.
   //shared pointers
   std::shared_ptr<Person> p = std::make_shared<Person>(); // here with this one you saved 1 allocation
-  // if you use new Person() u will be doing behind the hound 2 allocation 
-  //  because shared_ptr actually has to allocate block of memory 
+  // if you use new Person() u will be doing behind the hound 2 allocation
+  //  because shared_ptr actually has to allocate block of memory
   // called control  block that store reference count!
-  //  by using that standard way it do it at same time and 
+  //  by using that standard way it do it at same time and
   p->Print();
 
   std::weak_ptr<Person> e = std::make_shared<Person>(); // doesn't create control block but you can check if it's still valid before using it.
